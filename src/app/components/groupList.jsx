fa0a80c52b8da00b46bfa6ec4bed809a8b0aeb8e
Katya Bychkova <kats@list.ru>
2022-08-02 22:37:00 +0300

    перебор в groupList в зависимости от типа (массив/объект)

--- src/app/components/groupList.jsx
+++ src/app/components/groupList.jsx
@@ -9,26 +9,47 @@ const GroupList = ({
     selectedItem
 }) => {
     // console.log(items);
-    return (
-        // вариант перебора
-        <ul className="list-group">
-            {Object.keys(items).map((item) => (
-                <li
-                    key={items[item][valueProperty]}
-                    className={
-                        "list-group-item" +
-                        (items[item] === selectedItem ? " active" : "")
-                    }
-                    onClick={() => {
-                        onItemSelect(items[item]);
-                    }}
-                    role="button"
-                >
-                    {items[item][contentProperty]}
-                </li>
-            ))}
-        </ul>
-    );
+    if (Array.isArray(items)) {
+        return (
+            <ul className="list-group">
+                {items.map((item) => (
+                    <li
+                        key={item[valueProperty]}
+                        className={
+                            "list-group-item" +
+                            (item === selectedItem ? " active" : "")
+                        }
+                        onClick={() => {
+                            onItemSelect(item);
+                        }}
+                        role="button"
+                    >
+                        {item[contentProperty]}
+                    </li>
+                ))}
+            </ul>
+        );
+    } else {
+        return (
+            <ul className="list-group">
+                {Object.keys(items).map((item) => (
+                    <li
+                        key={items[item][valueProperty]}
+                        className={
+                            "list-group-item" +
+                            (items[item] === selectedItem ? " active" : "")
+                        }
+                        onClick={() => {
+                            onItemSelect(items[item]);
+                        }}
+                        role="button"
+                    >
+                        {items[item][contentProperty]}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
 };
 GroupList.defaultProps = {
     valueProperty: "_id",
@@ -36,7 +57,7 @@ GroupList.defaultProps = {
 };
 
 GroupList.propTypes = {
-    items: PropTypes.object.isRequired,
+    items: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired,
     valueProperty: PropTypes.string.isRequired,
     contentProperty: PropTypes.string.isRequired,
     onItemSelect: PropTypes.func,
