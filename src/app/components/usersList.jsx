31df9ddaaeed10de936e9fc0ad38c815603a36d6
Katya Bychkova <kats@list.ru>
2022-09-02 11:11:17 +0300

    рефакторинг searchValue -> searchQuery

--- src/app/components/usersList.jsx
+++ src/app/components/usersList.jsx
@@ -7,14 +7,13 @@ import GroupList from "./groupList";
 import api from "../api";
 import SearchStatus from "./searchStatus";
 import _ from "lodash";
-import SearchUserForm from "./searchUserForm";
 
 const UsersList = () => {
     const [currentPage, setCurrentPage] = useState(1);
     const [professions, setProfessions] = useState();
     const [selectedProf, setSelectedProf] = useState();
     const [sortBy, setSortBy] = useState({ iter: "name", order: "asc" }); // устанавливаем по какому признаку сортируем и тип сортировки
-    const [searchValue, setSearchValue] = useState(""); // хранение значений из строки поиска
+    const [searchQuery, setSearchQuery] = useState(""); // хранение значений из строки поиска
     const [users, setUsers] = useState(); // users - массив объектов из fakeApi
     const pageSize = 8;
 
@@ -43,10 +42,17 @@ const UsersList = () => {
 
     useEffect(() => {
         setCurrentPage(1);
-    }, [selectedProf]); // каждый раз, когда selectedProf будет меняться меняем значение currentPage = 1
+    }, [selectedProf, searchQuery]); // каждый раз, когда selectedProf будет меняться меняем значение currentPage = 1
 
     const handleProfessionSelect = (item) => {
+        if (searchQuery !== "") setSearchQuery(""); // очищаем строку поиска, если выбрана фильтрация по профессиям
         setSelectedProf(item);
+    };
+
+    // изменяет значения  строке поиска
+    const handleSearchQuery = ({ target }) => {
+        // setSelectedProf(undefined);
+        setSearchQuery(target.value);
     };
 
     const handlePageChange = (pageIndex) => {
@@ -57,30 +63,22 @@ const UsersList = () => {
         setSortBy(item);
     };
 
-    // изменяет значения  строке поиска
-    const handleSearchValue = (e) => {
-        console.log(e.target.value);
-        setSearchValue(e.target.value);
-    };
-
-    // выводит найденные значения на странице
-    // что сортируем -
-    // по какому признаку сортируем
-    // отсортированные значения
-
     if (users) {
         // приводим user.profession, selectedProf к одному типу тк объект ссылочный тип данных и они не равны друг другу
         // filteredUsers  - юзеры отфильтрованные по профессии, либо через search, либо все юзеры если фильтра нет
 
-        const filteredUsers = selectedProf
+        const filteredUsers = searchQuery
+            ? users.filter(
+                  (user) =>
+                      user.name
+                          .toLowerCase()
+                          .indexOf(searchQuery.toLowerCase()) !== -1
+              )
+            : selectedProf
             ? users.filter(
                   (user) =>
                       JSON.stringify(user.profession) ===
                       JSON.stringify(selectedProf)
-              )
-            : searchValue
-            ? users.filter((user) =>
-                  user.name.toLowerCase().includes(searchValue.toLowerCase())
               )
             : users;
 
@@ -115,10 +113,12 @@ const UsersList = () => {
                     )}
                     <div className="d-flex flex-column">
                         <SearchStatus length={count} />
-                        <SearchUserForm
-                            name="Search"
-                            value={searchValue}
-                            onChange={handleSearchValue}
+                        <input
+                            type="text"
+                            name="searchQuery"
+                            placeholder="Search..."
+                            onChange={handleSearchQuery}
+                            value={searchQuery}
                         />
                         {count > 0 && (
                             <UserTable
